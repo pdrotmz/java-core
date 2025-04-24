be2ee536f1ef2481007ef38b724d4168b049c79d
pdrotmz <pedrotomaz0210@gmail.com>
2025-04-24 14:55:18 -0300

    feat: adds arithmetic operations
    - Adds arithmetic operations
    - Adds Augmented Assignment Operators
    - Increment and Decrement Operators
    - Order of Operations

---
+++ src/arimethic/Arimethic.java
@@ -0,0 +1,51 @@
+package arimethic;
+
+public class Arimethic {
+
+    public static void main(String[] args) {
+
+        // TODO: Improve documentation
+
+        // Arithmetic Operators
+
+        int x = 10;
+        int y = 2;
+        int z;
+
+        // z = x + y;
+        // z = x - y;
+        // z = x * y;
+        // z = x / y;
+        // z = x % y;
+        // System.out.println(z);
+
+        // Augmented Assignment Operators
+
+        // x += y; // x += y it's the same thing of x = x + y
+        // x -= y; // x -= y it's the same thing of x = x - y
+        // x *= y; // x *= y it's the same thing of x = x * y
+        // x /= y; // x /= y it's the same thing of x = x / y
+        // x %= y; // x %= y it's the same thing of x = x % y
+        // System.out.println(x);
+
+        // Increment and Decrement Operators
+
+        // x++; // Increments x by 1, only per 1 in this case
+        // x--; // Decrements x by 1, only per 1 in this case
+        // System.out.println(x);
+
+
+        // Order of Operations [P-E-M-D-A-S]
+
+        // 1 - Parentheses
+        // 2 - Exponents
+        // 3 - Multiplications
+        // 4 - Divisions
+        // 5 - Additions
+        // 6 - Subtractions
+
+        double result = 3 + 4 * 7 + ( 5 + 2 ) / 2.5;
+
+        System.out.println(result);
+    }
+}
