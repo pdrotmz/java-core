6d512d3903f9fa5c984c6b37675db8ef0e70976d
pdrotmz <pedrotomaz0210@gmail.com>
2025-04-24 14:56:20 -0300

    feat: adds store challenger
    - Adds logic of store
    - Adds order and scanner to read dates

---
+++ src/shoppingcart/Store.java
@@ -0,0 +1,23 @@
+package shoppingcart;
+
+import java.util.Scanner;
+
+public class Store {
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+        System.out.print("What item would you like to buy ?: ");
+        String item = read.nextLine();
+        System.out.print("What is the price for each ?: ");
+        double price = read.nextDouble();
+        read.nextLine();
+        System.out.print("How many you like ?:");
+        int quantity = read.nextInt();
+
+        double totalPrice = price * quantity;
+
+        System.out.println("\nYou have bought " + item + "/s");
+        System.out.println("Your total is $" + totalPrice);
+    }
+}
